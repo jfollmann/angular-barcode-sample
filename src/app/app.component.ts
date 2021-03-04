0058f26ead9ead99a68c1569dc6669c5ad740c78
Jefferson Follmann <jeff.follmann@gmail.com>
2021-03-04 16:01:54 -0300

    Create sample project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+/* eslint-disable no-undef */
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {BarcodeScannerService} from 'src/services/barcodeScannerService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'barcode-sample';
+  title = 'barcode-scanner-sample';
+  barcode = 'No barcode read';
+  @ViewChild('input')
+  input!: ElementRef;
+
+  constructor() {
+    const barcodeScanner = new BarcodeScannerService();
+
+    barcodeScanner.scanned.subscribe(
+      code => (this.barcode = `Barcode read: ${code}`),
+      error => (this.barcode = `Error to read: ${error}`)
+    );
+  }
 }
